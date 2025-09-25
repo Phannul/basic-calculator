6cf1039ab9864b93ecdbc1cc84771a7ad3528e56
phannu <fanuelbirhane433@gmail.com>
2025-09-25 11:00:38 -0500

    final for BasicCalculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        String input = scanner.nextLine();
+
+        double firstNumber = Double.parseDouble(input);
+
+        System.out.println("Enter the Second Number: ");
+        String secondInput = scanner.nextLine();
+
+        double secondNumber = Double.parseDouble(secondInput);
+
+        System.out.println("Possible Operations are" +
+                "(A)ddition\n" +
+                "(S)ubtraction\n" +
+                "(M)ultiplication\n" +
+                "(D)ivision\n");
+        String operationChoice = scanner.nextLine();
+
+        double product = firstNumber * secondNumber;
+
+        System.out.println("The Product is " + product);
+
+        //System.out.println("The Result is: " + (input * secondInput));
+
+
+
+
+
+
+    }
 }
